186e110e35f29847da3648f64b0cc288eed084f3
vxaretra <andrewrizqi00@gmail.com>
2024-01-18 11:41:05 +0700

    update tasks list layout

--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -12,7 +12,7 @@ export default async function TasksPage() {
           <h1 className="text-5xl font-semibold">Tasks List</h1>
         </header>
         <Link className="w-fit px-2 border border-black" href={"/tasks/new"}>Add New Task +</Link>
-        <div className="p-4 border border-black flex flex-wrap justify-evenly">
+        <div className="p-4 border border-black grid grid-cols-3 justify-items-center gap-y-4">
           {tasks.map((task) => <TaskCard key={task.id} id={task.id} task={task.task} description={task.description} status={task.status} />)}
         </div>
       </div>
