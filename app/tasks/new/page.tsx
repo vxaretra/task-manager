9894023954e94c756b7c4859b0d32d815fc1cf51
vxaretra <andrewrizqi00@gmail.com>
2024-01-18 14:22:13 +0700

    fix new task layout

--- app/tasks/new/page.tsx
+++ app/tasks/new/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function TaskNewPage() {
   return (
-    <div className="w-1/3 mx-auto min-h-screen flex flex-col justify-center align-middle gap-5">
+    <div className="mx-auto min-h-screen max-w-md flex flex-col justify-center align-middle gap-5">
       <Link className="w-fit px-2 border border-black" href={"/tasks"}>Back</Link>
       <form className="border border-black flex flex-col p-4 gap-2" action={createNewTask}>
         <label htmlFor="task">Task:</label>
@@ -13,11 +13,11 @@ export default function TaskNewPage() {
         <p>Status:</p>
         <div>
           <input className="mr-1" type="radio" name="status" id="todo" value="TODO" required />
-          <label className="mr-2" htmlFor="todo">TODO</label>
+          <label className="mr-2 text-red-500 font-semibold" htmlFor="todo">TODO</label>
           <input className="mr-1" type="radio" name="status" id="inprogress" value="INPROGRESS" />
-          <label className="mr-2" htmlFor="inprogress">IN PROGRESS</label>
+          <label className="mr-2 text-orange-500 font-semibold" htmlFor="inprogress">IN PROGRESS</label>
           <input className="mr-1" type="radio" name="status" id="done" value="DONE" />
-          <label className="mr-2" htmlFor="done">DONE</label>
+          <label className="mr-2 text-green-500 font-semibold" htmlFor="done">DONE</label>
         </div>
         <button className="w-fit p-1 border border-black" type="submit">Add</button>
       </form>
