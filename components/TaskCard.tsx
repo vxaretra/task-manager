18ba60fc50a70e8892c8bdacf7aae6989816d83b
vxaretra <andrewrizqi00@gmail.com>
2024-01-18 14:30:08 +0700

    color code task status

--- components/TaskCard.tsx
+++ components/TaskCard.tsx
@@ -8,13 +8,19 @@ type TaskCardProps = {
   status: Status,
 };
 
+const taskStatusColor = {
+  [Status.TODO]: "text-red-500",
+  [Status.INPROGRESS]: "text-orange-500",
+  [Status.DONE]: "text-green-500",
+};
+
 export default function TaskCard({ id, task, description, status }: TaskCardProps) {
   return (
     <Link href={`/tasks/${id}`}>
       <div className="p-2 border border-black flex flex-col hover:cursor-pointer">
         <h1 className="text-xl font-bold">{task}</h1>
         <p className="text-ellipsis overflow-clip">{description}</p>
-        <p>{status}</p>
+        <p className={`${taskStatusColor[status]} font-semibold`}>{status}</p>
       </div>
     </Link>
   );
